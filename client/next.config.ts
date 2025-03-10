895c7cdee2c74d2e4312f65f90bc593e2b6bad03
Nishant Pradhan <nishant.pradhan2060@gmail.com>
2025-03-10 22:53:42 +0545

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [{hostname:"firebasestorage.googleapis.com"},{hostname:"https://lh3.googleusercontent.com"}],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
